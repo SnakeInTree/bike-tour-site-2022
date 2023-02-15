f0c13751c0310fc8418e971288e05d34b230ae9a
Kees  VandenBerg <kees@wisesystems.com>
2023-02-15 14:38:10 -0500

    fix reactivity stats

--- src/pages/components/Drawer/Stats.tsx
+++ src/pages/components/Drawer/Stats.tsx
@@ -40,14 +40,14 @@ const stats = {
 const StatsBlock = () => {
     return (
         <IconContext.Provider value={{size: "3.5rem", color: "#fff5f5"}}>
-            <div className="w-full bg-statgreen flex flex-row justify-center items-center sm:-translate-y-0 md:-translate-y-0 lg:-translate-y-52">
+            <div className="w-full bg-statgreen flex flex-row justify-center items-center sm:translate-y-4 md:translate-y-4 lg:-translate-y-52">
                 <div className="w-10/12 inline-flex flex-wrap justify-evenly">
                     <Stat statType={stats.distance} />
-                    <Stat statType={stats.days} />
                     <Stat statType={stats.highPt} />
+                    <Stat statType={stats.days} />
                     <Stat statType={stats.ascent} />
-                    <Stat statType={stats.numCountries} />
                     <Stat statType={stats.baguette} />
+                    <Stat statType={stats.numCountries} />
                 </div>
             </div>
         </IconContext.Provider>
@@ -56,11 +56,11 @@ const StatsBlock = () => {
 
 const Stat = ({statType}: any) => {
     return (
-        <div className="flex flex-col flex-wrap font-mapheader w-1/3 mb-8">
+        <div className="nil:w-full nil:content-start md:content-evenly sm:w-full md:w-1/2 lg:w-1/3 xl:w-1/3 flex flex-col flex-wrap font-mapheader mb-8">
             <span className="text-md font-semibold text-statgrey">{statType.name}</span>
-            <div className="flex flex-row align-middle items-center">
+            <div className="align-middle flex flex-row items-center">
                 {statType.icon}
-                <span className="ml-3 text-statblack font-extrabold text-2xl">{statType.value}</span>
+                <span className="ml-3 text-statblack font-extrabold text-2xl whitespace-nowrap">{statType.value}</span>
             </div>
         </div>
     );
