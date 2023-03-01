1b6b75abc516277ace72d07c00de4341773a1c97
Kees  VandenBerg <kees@wisesystems.com>
2023-03-01 11:30:29 -0500

    add footer styling

--- src/components/Drawer/Footer.tsx
+++ src/components/Drawer/Footer.tsx
@@ -1,6 +1,7 @@
 import {useDispatch} from "react-redux";
 import { IconContext } from "react-icons";
 import { CgArrowLongRightR, CgArrowLongLeftR } from "react-icons/cg";
+import { FaHome } from "react-icons/fa";
 import segmentData from "@/data/segments";
 import { updateActiveSegment } from "@/store/reducers/segmentList";
 
@@ -11,39 +12,62 @@ const Footer = ( {activeSegmentId}: {activeSegmentId: number} ) => {
     const nextSegment = activeSegmentId < 11 ? segmentData[activeSegmentId + 1] : null;
     
     const advanceSegmentId = () => activeSegmentId < 11 ? dispatch(updateActiveSegment(activeSegmentId + 1)) : null;
-    const regressSegmentId = () => activeSegmentId > 0 ? dispatch(updateActiveSegment(activeSegmentId - 1)) : null;
+    const regressSegmentId = () => activeSegmentId > -1 ? dispatch(updateActiveSegment(activeSegmentId - 1)) : null;
 
     return (
         <IconContext.Provider value={{size: "3rem"}}>
-            <div className="flex flex-row h-1/6 w-full text-2xl font-semibold font-mapheader cursor-pointer lg:-translate-y-20 ">   
-                <div 
-                    className="w-1/2 flex flex-row justify-center items-center transition-all ease-in-out duration-300 hover:underline"
-                    onClick={regressSegmentId}
-                >
-                    {prevSegment ? 
-                        (
-                            <>
-                                <CgArrowLongLeftR />
-                                <p className="mx-8">{prevSegment.name.toUpperCase()}</p>
-                                <prevSegment.icon />
-                            </>
-                        ) : null
-                    }
-                </div>
-                <div 
-                    className="w-1/2 flex flex-row justify-center items-center transition-all ease-in-out duration-300 hover:underline"
-                    onClick={advanceSegmentId}
-                >
-                    {nextSegment ? 
-                        (
-                            <>
-                                <nextSegment.icon />
-                                <p className="mx-8">{nextSegment.name.toUpperCase()}</p>
-                                <CgArrowLongRightR />
-                            </>
-                        ) : null
-                    }
-                </div>
+            <div className="flex flex-row h-1/6 w-full text-2xl font-semibold font-mapheader lg:-translate-y-20 ">   
+                {prevSegment ? 
+                    (
+                        
+                        <div 
+                            className="w-1/2 flex flex-row justify-center items-center cursor-pointer hover:underline"
+                            onClick={regressSegmentId}
+                        >
+                            <CgArrowLongLeftR />
+                            <p className="mx-8">{prevSegment.name.toUpperCase()}</p>
+                            <prevSegment.icon />
+                        </div>
+                    )
+                    :
+                    activeSegmentId === -1 ? 
+                            (
+                                <div className="w-1/2 flex flex-row justify-center items-center">
+                                    <p className="mx-8">THE ADVENTURE BEGINS:</p>
+                                </div>
+                            )
+                            :
+                            (
+                                <div 
+                                    className="w-1/2 flex flex-row justify-center items-center cursor-pointer hover:underline"
+                                    onClick={regressSegmentId}
+                                >
+                                    <CgArrowLongLeftR />
+                                    <p className="mx-8">HOME</p>
+                                    <FaHome />
+                                </div>
+                            )            
+                }
+                {nextSegment ? 
+                    (
+                        <div 
+                            className="w-1/2 flex flex-row justify-center items-center cursor-pointer hover:underline"
+                            onClick={advanceSegmentId}
+                        >
+                            <nextSegment.icon />
+                            <p className="mx-8">{nextSegment.name.toUpperCase()}</p>
+                            <CgArrowLongRightR />
+                        </div>
+                    ) 
+                    :
+                    (
+                        <div 
+                            className="w-1/2 flex flex-row justify-center items-center"
+                        >
+                            <p>AND THAT'S A WRAP :'-)</p>
+                        </div>
+                    )
+                }
             </div>
         </IconContext.Provider>
     );
