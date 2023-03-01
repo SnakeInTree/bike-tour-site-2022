d22d7c3bafce525ebba6fbd044a5fe4784e36aa8
Kees  VandenBerg <kees@wisesystems.com>
2023-03-01 12:39:26 -0500

    fix bad info wrapping

--- src/components/Drawer/InfoPanel.tsx
+++ src/components/Drawer/InfoPanel.tsx
@@ -4,69 +4,27 @@ import { CgArrowLongRightR } from "react-icons/cg";
 
 import { SectionContentList } from "@/data/text";
 import { InfoPanelSection, SectionContent } from "@/store/models";
-/**
- * InfoPanel is responsible for showcasing the following:
- * - Highlights
- * - Why did you do this?
- * - What kind of gear did you use? 
- * - Where did you sleep? 
- * - How can I do this too? 
- * - Trail Notes
- */
 
 const InfoPanel = () => {
     const [activeSectionId, setActiveSectionId] = useState<number>(0);
-    
     return (
-        <div className="flex flex-col w-full h-4/5">
+        <div className="flex flex-col">
             <SectionTitles activeSectionId={activeSectionId} setActiveSectionId={setActiveSectionId} />
             <SectionInfo activeSectionId={activeSectionId} />
         </div> 
     );
 };
 
-const SectionInfo = ({activeSectionId}: {activeSectionId: number}) => {
-    
-    const activeSection = SectionContentList[activeSectionId];
-
-    return (
-        <div className="w-full h-4/5 bg-headshotChevronBg py-8 px-16">
-            {activeSection.content.map((content: SectionContent) => <SectionInfoHeader key={content.header} content={content} />)}
-        </div>
-    );
-};
-
-const SectionInfoHeader = ({ content }: { content:SectionContent }) => {
-    const iconProps = {
-        size: "2rem",
-        color: "#A23D3B"
-    };
-    
-    return (
-        <IconContext.Provider value={iconProps}>
-            <div className="flex flex-row font-playfair w-10/12 pb-4 last:pb-0">
-                <div className="mr-4"><CgArrowLongRightR /></div>
-                <p className="inline-block"><strong className="text-lg font-semibold">{content.header}</strong> {content.content}</p>
-            </div>
-        </IconContext.Provider>
-       
-    );
-};
-
 const SectionTitles = ({activeSectionId, setActiveSectionId}: {activeSectionId: number, setActiveSectionId:Dispatch<SetStateAction<number>>}) => {
     return (
-        <div className="flex flex-col w-full h-1/6 align-middle justify-center items-center pt-12 bg-tan drop-shadow-md">
-            <div className="flex flex-row w-5/6 h-1/12">
-                {SectionContentList.map((content:InfoPanelSection) => <SectionHeader key={content.title} content={content} activeSectionId={activeSectionId} setActiveSectionId={setActiveSectionId}/>)}
-            </div>
+        <div className="flex flex-row align-middle sm:justify-around md:justify-around lg:justify-center items-center pt-12 bg-tan drop-shadow-md">
+            {SectionContentList.map((content:InfoPanelSection) => <SectionTitle key={content.title} content={content} activeSectionId={activeSectionId} setActiveSectionId={setActiveSectionId}/>)}
         </div>
     );
 };
 
-const SectionHeader = ({activeSectionId, setActiveSectionId, content}: {content:InfoPanelSection, activeSectionId: number, setActiveSectionId:Dispatch<SetStateAction<number>>}) => {
-    //const before = "before:content-[''] before:border-y-42 before:border-l-24 before:border-l-statgrey before:border-y-transparent before:absolute before:rotate-90 before:-translate-y-8";
+const SectionTitle = ({activeSectionId, setActiveSectionId, content}: {content:InfoPanelSection, activeSectionId: number, setActiveSectionId:Dispatch<SetStateAction<number>>}) => {
     const isActive = activeSectionId === content.sectionId;
-
     const handleClick = () => setActiveSectionId(content.sectionId);
     const iconProps = {
         size: "3rem",
@@ -75,12 +33,37 @@ const SectionHeader = ({activeSectionId, setActiveSectionId, content}: {content:
 
     return (
         <IconContext.Provider value={iconProps}>
-            <div className="flex flex-col w-full items-center" onClick={handleClick}>
+            <div className="flex flex-col items-center" onClick={handleClick}>
                 <div className={`nil:hidden sm:hidden md:hidden lg:block relative p-4 text-center font-mapheader text-lg ${isActive ? "bg-statgrey text-tan" : "text-segmentHoverBg hover:text-statblack ease-in-out"} font-semibold whitespace-nowrap`}>{content.title.toUpperCase()}</div>
                 <div className={`nil:block sm:block md:block lg:hidden relative p-4 text-center z-450 ${isActive ? "bg-statgrey" : ""}`}><content.Icon /></div>
             </div>
         </IconContext.Provider>
         
+    );
+};
+
+const SectionInfo = ({activeSectionId}: {activeSectionId: number}) => {
+    const activeSection = SectionContentList[activeSectionId];
+    return (
+        <div className="w-full h-4/5 bg-headshotChevronBg py-8 pl-12 pr-16">
+            {activeSection.content.map((content: SectionContent) => <SectionInfoContent key={content.header} content={content} />)}
+        </div>
+    );
+};
+
+const SectionInfoContent = ({ content }: { content:SectionContent }) => {
+    const iconProps = {
+        size: "2rem",
+        color: "#A23D3B"
+    };
+    return (
+        <IconContext.Provider value={iconProps}>
+            <div className="flex flex-row font-playfair w-10/12 pb-4 last:pb-0">
+                <div className="mr-4"><CgArrowLongRightR /></div>
+                <p className="inline-block"><strong className="text-lg font-semibold">{content.header}</strong> {content.content}</p>
+            </div>
+        </IconContext.Provider>
+       
     );
 };
 
